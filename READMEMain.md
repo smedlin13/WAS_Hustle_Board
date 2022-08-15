b5ea00c36eadf6ac6f4beaf89d2978a7b7873546
Stephanie Medlin <94500119+smedlin13@users.noreply.github.com>
2022-08-15 10:19:37 -0600

    see what happens

---
+++ READMEMain.md
@@ -0,0 +1,18 @@
+README
+The WAS Hustle Board is for Wasatch Adaptive Sports' annual event that tracks mileage while raising money in support of providing adaptive recreation.
+
+Built using Ruby version 3.1.1
+
+This site was built using React.js
+
+Gems installed: Devise, Pry, Rails 7.0.2
+
+To run:
+
+```WAS_Hustle_Board % bundle exec rails s -p 3001```
+
+split your terminal and run
+
+```WAS_Hustle_Board % cd client```
+
+```client % yarn start```
---
+++ app/controllers/api/mileages_controller.rb
@@ -0,0 +1,43 @@
+class Api::MileagesController < ApplicationController
+  before_action :set_mileage, only: [:show, :update, :destroy]
+
+  def index
+    render json: current_user.mileages
+  end
+  
+  def show
+    render json: @mileage
+  end
+
+  def create
+    @mileage = current_user.mileages.new(mileage_params)
+    if @mileage.save
+      render json: @mileage
+    else 
+      render json: { errors: @mileage.errors } , status: :unprocessable_entity
+    end
+  end
+
+  def update
+    if @mileage.update(mileage_params)
+      render json: @mileage
+    else
+      render json: { errors: @mileage.errors }, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @mileage.destroy
+    render json: { message: 'Mileage entry deleted'}
+  end
+
+  private
+  def mileage_params
+    params.require(:mileage).permit(:miles, :date)
+  end
+
+  def set_mileage
+    @mileage = current_user.mileages.find(params[:id])
+  end
+
+end
